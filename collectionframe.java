d5d9aef5aad6315d38d809517ba22507e4163ef4
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-03-16 15:35:51 +0530

    ArrayList

---
+++ collectionframe.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class collectionframe {
+    public static void main(String[] args) {
+            System.out.println("Collection Framework:");
+            //ArrayList
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(1);
+            list.add(2);
+            list.add(3);
+            list.add(4);
+            list.add(5);
+            System.out.println("ArrayList: " + list);
+            
+    }
+}
