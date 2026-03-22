9f6056bfd396f76742bcb87f6c997b2d3becde97
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-03-22 14:24:42 +0530

    Practicing ArrayLIst Questions

---
+++ ArrayL.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class ArrayL {
+    public static void main(String[] args) {
+        // Question-1
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(04);
+        numbers.add(10);
+        numbers.add(2005);
+        System.out.println(" Numbers list : "+numbers);
+    }
+}
