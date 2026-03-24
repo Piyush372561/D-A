e2150f73df4abf191d2fc9fa6d88258362ed7b45
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-03-24 22:24:57 +0530

    Comparator details

--- Comparator.java
+++ Comparator.java
@@ -1,5 +1,6 @@
 public class Comparator {
     public static void main(String[] args) {
+        System.out.println("Comparator is an Interface jiski help se hum costum ordering kr skte hai.");
         
     }
 }
