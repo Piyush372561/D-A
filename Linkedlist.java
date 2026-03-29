41933ed7e1f592f5741212862ce71b6b8f627f54
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-03-29 17:25:30 +0530

    Create linked list on the fly

--- Linkedlist.java
+++ Linkedlist.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.LinkedList;
+
 public class Linkedlist {
     public static void main(String[] args) {
         LinkedList<Integer> linkedlist = new LinkedList<>();
@@ -9,9 +11,22 @@ public class Linkedlist {
         System.out.println("Element at index 2 : "+linkedlist.get(2));
         linkedlist.addFirst(0);
         linkedlist.addLast(5);
+        System.out.println("Element at index 2 after adding elements: "+linkedlist.get(2));
 
         System.out.println(linkedlist);
         System.out.println("First element of the linkedlist : "+linkedlist.getFirst());
         System.out.println("First element of the linkedlist : "+linkedlist.getLast());
+
+        //remove function in LinkedList
+        linkedlist.removeIf(x -> x%2 == 0);
+        System.out.println("List after removing even numbers: "+linkedlist);
+
+        LinkedList<String> animals = new LinkedList<>(Arrays.asList("Parrot", "Dog", "Cat", "Cow", "Tiger", "Elephant"));
+        LinkedList<String> animalsremoved = new LinkedList<>(Arrays.asList("Dog", "Cat"));
+        animals.removeAll(animalsremoved);
+        System.out.println(animals);
+
+
+
     }
 }
