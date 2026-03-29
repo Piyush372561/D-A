5462ba6116691570c814259a9906e5cbc2931746
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-03-29 17:05:33 +0530

    First and last element

--- Linkedlist.java
+++ Linkedlist.java
@@ -6,10 +6,12 @@ public class Linkedlist {
         linkedlist.add(2);
         linkedlist.add(3);
         linkedlist.add(4);
-        System.out.println(linkedlist.get(2));
+        System.out.println("Element at index 2 : "+linkedlist.get(2));
         linkedlist.addFirst(0);
         linkedlist.addLast(5);
 
         System.out.println(linkedlist);
+        System.out.println("First element of the linkedlist : "+linkedlist.getFirst());
+        System.out.println("First element of the linkedlist : "+linkedlist.getLast());
     }
 }
